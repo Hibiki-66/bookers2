71079d1876eb75ca78d5d11555c85b87c897d0c0
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2025-10-21 12:43:39 +0000

    [Finish]名前でログインできるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
 
+  devise_for :users
+
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
   resources :users, only: [:index, :show, :edit]
-  
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
