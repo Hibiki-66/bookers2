6517ccaddd25f9b3b05723c87fdcab6f8de1a270
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2025-10-22 11:29:38 +0000

    edit finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,13 +13,17 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
-  def update
-  end
-
   def show
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id) 
   end
 
   def destroy
@@ -28,6 +32,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:name, :image, :introduction)
+    params.require(:book).permit(:title, :body)
   end
 end
