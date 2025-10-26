e25818ef6c6ea093acacf68c4fb07fa79741f93e
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2025-10-26 10:09:35 +0000

    show end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,10 +11,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -24,10 +25,13 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id) 
+    redirect_to book_path(@book.id) 
   end
 
   def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path  # 投稿一覧画面へリダイレクト
   end
   
   private
