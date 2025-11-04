501be146c4e4061ea8211a994449b3f413920564
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2025-11-04 20:25:15 +0000

    [finish]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,20 @@ class BooksController < ApplicationController
   # end
 
   def index
-    @book = Book.new
+    @book_new = Book.new
     @books = Book.all
     @user = current_user
   end
 
   def create
     @book_new = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    @book_new.user_id = current_user.id
+    if @book_new.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book_new.id)
+    else
+      render :index
+    end
   end
 
   def show
@@ -27,8 +31,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(@book.id) 
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
